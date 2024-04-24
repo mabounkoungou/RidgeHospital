d8abbf30d116c7b02bf751cb188bbf6d9602a430
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-24 07:02:14 +0000

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-om7iKLa5cK8WZb46J75xT3BlbkFJm7XNeH93fFt9MSLCjvx3"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
