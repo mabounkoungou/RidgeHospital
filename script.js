6a61b356fd9fcfe2e61590a36a0246a81cc9356b
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-24 11:49:38 +0000

    update

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ function SendMail(){
         email : document.getElementById("email").value,
         subject : document.getElementById("subject").value,
     }
-    emailjs.send("service_s5egfm5", "template_bgysfm1", parms).then(alert("Email Sent Successfully"))
+    emailjs.send("service_s5egfm5", "template_bgysfm1", parms)
+        .then(function() {
+            alert("Email Sent Successfully");
+            // Clear the input fields
+            document.getElementById("message").value = "";
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+        })
+        .catch(function(error) {
+            console.error('Email send failed:', error);
+            // Optionally handle the error here
+        });
 }
